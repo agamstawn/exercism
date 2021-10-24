0e3fd1d7adfeb5d7fe98f315c4e7839a0bc4a821
agamstawn <agam.setiawan@nutrifood.co.id>
2021-10-24 23:23:50 +0700

    [Basic I - Elixir] Raindrops

---
+++ elixir/raindrops/lib/raindrops.ex
@@ -0,0 +1,30 @@
+defmodule Raindrops do
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @spec convert(pos_integer) :: String.t()
+  def convert(number) do
+   result = ""
+    if rem(number, 3) == 0 do
+      result = result <> "Pling"
+    end
+    if rem(number, 5) == 0 do
+      result = result <> "Plang"
+    end
+    if rem(number, 7) == 0 do
+      result = result <> "Plong"
+    end
+  
+    if String.length(result) > 0 do
+      "#{result}"
+    else
+      "#{number}"
+    end
+  end
+end
