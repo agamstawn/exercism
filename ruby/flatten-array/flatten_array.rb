03e2486debade810b679957aa7097dab6adacccf
agamstawn <agam.setiawan@nutrifood.co.id>
2021-10-27 20:13:42 +0700

    [Basic I - Ruby] Flatten Array (Revised)

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,5 +1,14 @@
 class FlattenArray
-  def self.flatten(arr_params)
-    arr_params.flatten.compact
+  def self.flatten(arr_params, arr_result = [])
+    # arr_params.flatten.compact
+     arr_params.each do |element|
+      next if element.nil?
+      if element.is_a?(Array)
+        flatten(element, arr_result) 
+      else
+        arr_result << element
+      end 
+    end
+    arr_result
   end
 end
